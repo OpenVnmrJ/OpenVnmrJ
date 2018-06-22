7b41b8bf7c571957584447dbdee49cc5c4f5cbd3
Dan Iverson <daniverson2016@gmail.com>
2018-06-22 12:50:02 -0700

    Updated accounting to support not cutting records (#185)
    A new option to doAcct will just extract records without cutting
    the master list. See the updated manual/vnmr_accounting
    Also, the scipts passed the "shellcheck" utility

--- src/scripts/doAcct.sh
+++ src/scripts/doAcct.sh
@@ -8,12 +8,12 @@
 # For more information, see the LICENSE file.
 #
 # set -x
-if [ x$vnmrsystem = "x" ]
+if [ x"$vnmrsystem" = "x" ]
 then
    vnmrsystem=/vnmr
 fi
 admin=$($vnmrsystem/bin/fileowner $vnmrsystem/vnmrrev)
-if [ $USER != $admin ]
+if [ "$USER" != "$admin" ]
 then
    echo "OpenVnmrj accounting tool can only be used by the"
    echo "OpenVnmrJ administrator account $admin"
@@ -23,20 +23,43 @@ fi
 saveDate=$(date +"%Y_%m_%d")
 saveDir=$HOME/vnmrsys/accounting/${saveDate}
 
-$vnmrsystem/jre/bin/java -jar $vnmrsystem/java/account.jar -saveDir ${saveDir} -cut
-$vnmrsystem/jre/bin/java -jar $vnmrsystem/java/account.jar -saveDir ${saveDir} -in ${saveDir}/acctLog.xml -summary -summaryCsv -acctCsv
+eYear=$(date +"%Y")
+eMon=$(date +"%m")
+day=$(date +"%d")
+if [[ $eMon == "01" ]];
+then
+   sMon=12
+   sYear=$((eYear-1))
+else
+   sMon=$((eMon-1))
+   sYear=$eYear
+fi
+startDate=$(date -d "$sYear"/"$sMon"/"$day" +"%b %d %Y")
+endDate=$(date +"%b %d %Y")
+
+if [[ $# -gt 0 ]] ;
+then
+   $vnmrsystem/jre/bin/java -jar $vnmrsystem/java/account.jar -saveDir "${saveDir}" -cut
+   $vnmrsystem/jre/bin/java -jar $vnmrsystem/java/account.jar -saveDir "${saveDir}" \
+            -in "${saveDir}"/acctLog.xml -summary -summaryCsv -acctCsv
+else
+   $vnmrsystem/jre/bin/java -jar $vnmrsystem/java/account.jar -saveDir "${saveDir}" \
+            -startDate "${startDate}" -endDate "${endDate}" -summary -summaryCsv -acctCsv
+fi
 
 host=$(hostname)
-curDate=$(date +"%b %d %Y")
 
-cat $vnmrsystem/adm/accounting/acctHeader.ps | sed "s/HOSTNAME/${host}/" | sed "s/DATE/${curDate}/" > ${saveDir}/summary.ps
+sed "s/HOSTNAME/${host}/" $vnmrsystem/adm/accounting/acctHeader.ps |
+                sed "s/ENDDATE/${endDate}/" |
+                sed "s/STARTDATE/${startDate}/" > "${saveDir}"/summary.ps
+
 filename=${saveDir}/summary.txt
 while IFS='' read -r line || [[ -n "$line" ]]; do
-   echo "($line) show" >> ${saveDir}/summary.ps
-   echo newline >> ${saveDir}/summary.ps
-done < $filename
-cat $vnmrsystem/adm/accounting/acctTail.ps >> ${saveDir}/summary.ps
+   echo "($line) show" >> "${saveDir}"/summary.ps
+   echo newline >> "${saveDir}"/summary.ps
+done < "$filename"
+cat $vnmrsystem/adm/accounting/acctTail.ps >> "${saveDir}"/summary.ps
 
-convert ${saveDir}/summary.ps -flatten ${saveDir}/summary.png
+convert "${saveDir}"/summary.ps -flatten "${saveDir}"/summary.png
 
 exit 0
