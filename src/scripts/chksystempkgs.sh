3514754bc64146edb62c2b65f5a4b166c1ed2703
Dan Iverson <daniverson2016@gmail.com>
2019-07-30 15:09:44 -0700

    Updated the CentOS and Ubuntu pckage installation. (#277)
    This works on CentOS 7.4, 7.5, and 7.6. It also works on
    Ubuntu 14, 16, and 18. It fixes Issues #22 #23 and #67

--- src/scripts/chksystempkgs.sh
+++ src/scripts/chksystempkgs.sh
@@ -17,24 +17,26 @@ status=0
 # rpm (RedHat and CentOS) vs dpkg (Debian)
 #
 if [ ! -x /usr/bin/dpkg ]; then
+   echo "Checking for RHEL / CentOS packages required by OpenVnmrJ"
    if [ -f /etc/centos-release ]; then
       rel=centos-release
    else
       rel=redhat-release
    fi
    version=$(rpm -q $rel | cut -d'-' -f3)
-   packagecommonlist='make gcc gcc-c++ gdb libtool binutils automake strace autoconf glibc-devel expect rsh-server xinetd tftp-server kernel-headers kernel-devel libidn libXp libXmu mutt sendmail-cf k3b ghostscript ImageMagick'
+   packagecommonlist='make gcc gcc-c++ gdb libtool binutils automake strace autoconf glibc-devel expect rsh-server tftp-server mutt k3b ghostscript ImageMagick'
    if [ $version -eq 7 ]
    then
 #     for RHEL 7.X must list 32-bit packages, since these are no longer installed with the 64-bit versions
       package71list='libgfortran motif'
-      package32Bitlist='rsh libXau.i686 libX11.i686 libXext-devel.i686 libXtst-devel.i686 libXi.i686 libstdc++.i686 libstdc++-devel.i686 glibc.i686 glibc-devel.i686 libXt.i686 libXt-devel.i686 ncurses-libs.i686 ncurses-devel.i686 motif.i686 motif-devel.i686 mesa-libGL-devel mesa-libGL-devel.i686 atk.i686 gtk2.i686 mesa-libGL mesa-libGL.i686 mesa-libGLU mesa-libGLU.i686 libidn.i686 libxml2.i686 expat.i686 libXaw.i686 libXpm.i686'
+      package32Bitlist='rsh libstdc++.i686 libstdc++-devel.i686 glibc.i686 glibc-devel.i686 mesa-libGL-devel mesa-libGL mesa-libGLU'
+      package32Bitlist='rsh libstdc++.i686 libstdc++-devel.i686 glibc.i686 glibc-devel.i686'
       packagelist="$packagecommonlist $package71list $package32Bitlist"
    elif [ $version -eq 6 ]
    then
 #     for RHEL 6.X must list 32-bit packages, since these are no longer installed with the 64-bit versions
       package61list='libgfortran openmotif22 gnome-power-manager'
-      package32Bitlist='rsh libXau.i686 libX11.i686 libXext-devel.i686 libXtst-devel.i686 libXi.i686 libstdc++.i686 libstdc++-devel.i686 glibc.i686 glibc-devel.i686 libXt.i686 libXt-devel.i686 ncurses-libs.i686 ncurses-devel.i686 openmotif.i686 openmotif-devel.i686 mesa-libGL-devel mesa-libGL-devel.i686 atk.i686 gtk2.i686 mesa-libGL mesa-libGL.i686 mesa-libGLU mesa-libGLU.i686 libidn.i686 libxml2.i686 expat.i686 libXaw.i686 libXpm.i686'
+      package32Bitlist='rsh libstdc++.i686 libstdc++-devel.i686 glibc.i686 glibc-devel.i686 openmotif openmotif-devel mesa-libGL-devel mesa-libGL mesa-libGLU'
       packagelist="$packagecommonlist $package61list $package32Bitlist"
    else
       packagecommonlist='compat-libf2c-34 compat-gcc-34-g77 openmotif22 gnome-power-manager'
@@ -44,12 +46,10 @@ if [ ! -x /usr/bin/dpkg ]; then
    fi
 
 else
-   # In Ubuntu 11 & 12 the ia32-sun-java6-bin package has been dropped from the repositories due to licensing issues with Oracle
-   #   12.04
+   echo "Checking for Ubuntu / Debian packages required by OpenVnmrJ"
    distrover=$(lsb_release -rs)
-   #   12
    distmajor=${distrover:0:2}
-   packagecommonlist='csh make gcc expect ethtool rarpd rsh-server tftpd openssh-server mutt sharutils sendmail-cf gnome-power-manager k3b kdiff3 ghostscript imagemagick'
+   packagecommonlist='csh make gcc gfortran expect openssh-server mutt sharutils sendmail-cf gnome-power-manager kdiff3 ghostscript imagemagick'
    if [ $distmajor -ge 16 ] ; then
      packageXlist='openjdk-8-jre bc libmotif-dev'
    elif [ $distmajor -ge 14 ] ; then
--- src/scripts/installpkgs.sh
+++ src/scripts/installpkgs.sh
@@ -10,16 +10,6 @@
 #
 # set -x
 
-rel=centos-release
-if [ ! -f /etc/centos-release ]; then
-   if [ ! -f /etc/redhat-release ]; then
-      echo "$0 can only be used for CentOS or RedHat systems"
-      exit 1
-   else
-      rel=redhat-release
-   fi
-fi
-version=`rpm -q $rel | cut -d'-' -f3`
 ping -W 1 -c 1 www.github.com > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
@@ -27,24 +17,36 @@ then
    exit 1
 fi
 
+if [ ! -x /usr/bin/dpkg ]; then
+ rel=centos-release
+ if [ ! -f /etc/centos-release ]; then
+   if [ ! -f /etc/redhat-release ]; then
+      echo "$0 can only be used for CentOS or RedHat systems"
+      exit 1
+   else
+      rel=redhat-release
+   fi
+ fi
+ version=`rpm -q $rel | cut -d'-' -f3`
+
 # Determine if some packages are already installed. If they are, do not remove them.
 
 # perl-homedir creates a perl5 directory in every acct. Removing it fixes it so it does not do that.
-perlHomeInstalled=0
-if [ "$(rpm -q perl-homedir | grep 'not installed' > /dev/null;echo $?)" != "0" ]
-then
+ perlHomeInstalled=0
+ if [ "$(rpm -q perl-homedir | grep 'not installed' > /dev/null;echo $?)" != "0" ]
+ then
    perlHomeInstalled=1
-fi
-epelInstalled=0
-if [ "$(rpm -q epel-release | grep 'not installed' > /dev/null;echo $?)" != "0" ]
-then
+ fi
+ epelInstalled=0
+ if [ "$(rpm -q epel-release | grep 'not installed' > /dev/null;echo $?)" != "0" ]
+ then
    epelInstalled=1
-fi
-turbovncFileInstalled=0
-if [ -f /etc/yum.repos.d/TurboVNC.repo ]
-then
+ fi
+ turbovncFileInstalled=0
+ if [ -f /etc/yum.repos.d/TurboVNC.repo ]
+ then
    turbovncFileInstalled=1
-fi
+ fi
 
 #for RHEL 7 and Centos 7
 # remove gnome-power-manager tk tcl
@@ -53,7 +55,13 @@ fi
 #   and add libgfortran
 # recompile Xrecon to remove libtiff dependence
 
-commonList='
+# kernel-headers
+# kernel-devel
+# libidn
+# sendmail-cf
+# xinetd
+
+ commonList='
   make
   gcc
   gcc-c++
@@ -63,52 +71,30 @@ commonList='
   automake
   strace
   autoconf
-  glibc-devel
   expect
   rsh
   rsh-server
-  xinetd
   tftp-server
-  kernel-headers
-  kernel-devel
-  libidn
   libgfortran
   mutt
-  sendmail-cf
   ghostscript
-'
+ '
 
 # Must list 32-bit packages, since these are no longer installed along with the 64-bit versions
-bit32List='
-  libXau.i686
-  libX11.i686
-  libXi.i686
-  libstdc++.i686
-  glibc.i686
-  libXt.i686
-  ncurses-libs.i686
+ bit32List='
   mesa-libGL-devel
-  atk.i686
-  gtk2.i686
   mesa-libGL
-  mesa-libGL.i686
   mesa-libGLU
   mesa-libGLU-devel
-  mesa-libGLU.i686
-  libidn.i686
-  libxml2.i686
-  expat.i686
-  libXaw.i686
-  libXpm.i686
-'
+ '
 
-pipeList='
+ pipeList='
   xorg-x11-fonts-100dpi
   xorg-x11-fonts-ISO8859-1-100dpi
   xorg-x11-fonts-75dpi
   xorg-x11-fonts-ISO8859-1-75dpi
   xorg-x11-fonts-misc
-'
+ '
 
 # The following were removed from the CentOS 6.8 kickstart
 # gnome-python2-desktop
@@ -134,45 +120,46 @@ pipeList='
 # @tex
 # @virtualization
 # @workstation-policy  installs gdm, already present
+# @additional-devel
+# @base
+# @core
+# @desktop-debugging
+# @development
+# @directory-client
+# @emacs
+# @fonts
+# @graphical-admin-tools
+# @input-methods
+# @internet-browser
+# @java-platform
+# @legacy-x
+# @network-file-system-client
+# @performance
+# @perl-runtime
+# @print-client
+# @remote-desktop-clients
+# @technical-writing
+# @virtualization-client
+# @virtualization-platform
+# @x11
+# libgnomeui-devel
+# libbonobo-devel
+# kdegraphics
 
-package68List=' 
-  @additional-devel
-  @base
-  @core
+ package68List=' 
   @debugging
-  @desktop-debugging
-  @development
-  @directory-client
-  @emacs
-  @fonts
-  @graphical-admin-tools
   @graphics
-  @input-methods
-  @internet-browser
-  @java-platform
-  @legacy-x
-  @network-file-system-client
-  @performance
-  @perl-runtime
-  @print-client
-  @remote-desktop-clients
-  @technical-writing
-  @virtualization-client
-  @virtualization-platform
-  @x11
-'
-item68List='
+  libstdc++
+  libstdc++-devel
+  glibc
+  glibc-devel
+ '
+
+ offList=' 
   libgcrypt-devel
   libXinerama-devel
-  motif
-  motif-devel
-  motif.i686
-  motif-devel.i686
-  libXmu-devel
   xorg-x11-proto-devel
   startup-notification-devel
-  libgnomeui-devel
-  libbonobo-devel
   junit
   libXau-devel
   libXrandr-devel
@@ -181,7 +168,6 @@ item68List='
   libxslt-devel
   libglade2-devel
   gnutls-devel
-  mtools
   pax
   python-dmidecode
   oddjob
@@ -205,69 +191,89 @@ item68List='
   libXp
   perl-DBD-SQLite
   libvirt-java
+  python-psycopg2
+  xinetd
+  xterm
+  createrepo
+  gconf-editor
+  gsl-devel
+  hplip-gui
+  icedtea-web
+  lm_sensors
+  logwatch
+  recode
+  syslinux-extlinux
+  perl-Compress-Raw-Bzip2
+ '
+
+ item68List='
+  motif
+  mtools
   expect
   gpm
   ImageMagick
   k3b
-  kdegraphics
   minicom
   pexpect
   postgresql-docs
   postgresql-odbc
   postgresql-server
   PyGreSQL
-  python-psycopg2
   sharutils
   telnet
   tftp-server
-  xinetd
-  kde-baseapps-libs
-  xterm
+  a2ps
+  compat-libstdc++-33
+  dos2unix
+  gitk
+  gnuplot
+  samba
+  tftp
+  fuseiso
+ '
+#These do not need to be installed for running OpenVnmrJ
+#Just if you intend to build it
+ buildList='
+  motif-devel
+  libXmu-devel
+  libXaw-devel.i686
+  libXext-devel.i686
+  libX11-devel.i686
+  libXau.i686
+  libX11.i686
+  libXi.i686
+  libXt.i686
+  libXaw.i686
+  libXpm.i686
+  libXt-devel.i686
+  libXtst-devel.i686
+  ncurses-libs.i686
+  ncurses-devel.i686
+  libxml2.i686
+  libxml2-devel.i686
+  alsa-lib.i686
   atk-devel.i686
-  expat-devel.i686
   glibc-devel.i686
   gtk2-devel.i686
   libidn-devel.i686
   libstdc++-devel.i686
-  libXaw-devel.i686
-  libXext-devel.i686
-  libxml2-devel.i686
-  libXt-devel.i686
-  libXtst-devel.i686
-  mesa-libGL-devel.i686
-  mesa-libGLU-devel.i686
-  ncurses-devel.i686
-  a2ps
-  alsa-lib.i686
-  compat-libstdc++-33
-  createrepo
-  dos2unix
-  gconf-editor
-  gitk
-  gnuplot
-  gsl-devel
-  hplip-gui
-  icedtea-web
-  libX11-devel.i686
-  lm_sensors
-  logwatch
-  recode
-  samba
-  syslinux-extlinux
-  tftp
-  perl-Compress-Raw-Bzip2
-  fuseiso
-'
+  expat.i686
+  expat-devel.i686
+  kde-baseapps-libs
+  atk.i686
+  gtk2.i686
+  libidn.i686
+ '
 # from epel-release
-epelList='
+ epelList='
   scons
   meld
   x11vnc
   ntfsprogs
   fuse-ntfs-3g
   kdiff3
-'
-if [ $version -ne 7 ]; then
+ '
+ if [ $version -ne 7 ]; then
 #  Add older motif packages
    ver6List='
       openmotif22
@@ -275,65 +281,68 @@ if [ $version -ne 7 ]; then
       openmotif-devel.i686
    '
    packagelist="$ver6List $item68List $commonList $bit32List $pipeList"
-else
+ else
    packagelist="$item68List $commonList $bit32List $pipeList"
-fi
+   packagelist="$item68List $commonList $pipeList"
+ fi
 
-postfix=`date +"%F_%T"`
-if [ -d /tmp/ovj_preinstall ]; then
+ postfix=`date +"%F_%T"`
+ if [ -d /tmp/ovj_preinstall ]; then
    logfile="/tmp/ovj_preinstall/pkgInstall.log_$postfix"
-else
+ else
    logfile="/tmp/pkgInstall.log_$postfix"
-fi
+ fi
 
 # The PackageKit script often holds a yum lock.
 # This prevents this script from executing
-npids=$(ps -ef  | grep PackageKit | grep -v grep | awk '{ printf("%d ",$2) }')
-for prog_pid in $npids
-do
-   kill -s 3 $prog_pid
-done
+ npids=$(ps -ef  | grep PackageKit | grep -v grep | awk '{ printf("%d ",$2) }')
+ for prog_pid in $npids
+ do
+   kill $prog_pid
+ done
 
-echo "You can monitor the progress in a separate terminal window with the command"
-echo "tail -f $logfile"
-echo "Installing standard packages (1 of 3)"
-yum -y install $package68List &> $logfile
+ echo "You can monitor the progress in a separate terminal window with the command"
+ echo "tail -f $logfile"
+ echo "Installing standard packages (1 of 3)"
+ yum -y install $package68List &> $logfile
+ yum -y upgrade glibc glibc-devel libstdc++ libstdc++-devel &>> $logfile
 
-echo "Installing required packages (2 of 3)"
-yumList=''
-for xpack in $packagelist
-do
+ echo "Installing required packages (2 of 3)"
+ yumList=''
+ for xpack in $packagelist
+ do
 #  rpm -q $xpack
    if [ "$(rpm -q $xpack | grep 'not installed' > /dev/null;echo $?)" == "0" ]
    then
 #     echo "VnmrJ required RHEL package \"$xpack\" NOT installed"
       yumList="$yumList $xpack"
    fi
-done
-if [ "x$yumList" != "x" ]; then
+ done
+ if [ "x$yumList" != "x" ]; then
    yum -y install $yumList &>> $logfile
-fi
+ fi
+ yum -y install glibc.i686 glibc-devel.i686 libstdc++.i686 libstdc++-devel.i686 &>> $logfile
 
 # perl-homedir creates a perl5 directory in every acct. This fixes it so it does not do that.
-if [ $perlHomeInstalled -eq 0 ]
-then
+ if [ $perlHomeInstalled -eq 0 ]
+ then
    echo "Removing perl-home rpm" &>> $logfile
    yum -y erase perl-homedir &>> $logfile
-fi
+ fi
 
-echo "Installing additional packages (3 of 3)"
-if [ $epelInstalled -eq 0 ]
-then
+ echo "Installing additional packages (3 of 3)"
+ if [ $epelInstalled -eq 0 ]
+ then
    yum -y install epel-release &>> $logfile
-fi
-yum -y install $epelList &>> $logfile
-if [ $epelInstalled -eq 0 ]
-then
+ fi
+ yum -y install $epelList &>> $logfile
+ if [ $epelInstalled -eq 0 ]
+ then
    yum -y erase epel-release &>> $logfile
-fi
+ fi
 #Add turbovnc
-if [ "$(rpm -q turbovnc | grep 'not installed' > /dev/null;echo $?)" == "0" ]
-then
+ if [ "$(rpm -q turbovnc | grep 'not installed' > /dev/null;echo $?)" == "0" ]
+ then
    if [ $turbovncFileInstalled -eq 0 ]
    then
       cat >/etc/yum.repos.d/TurboVNC.repo  <<EOF
@@ -352,24 +361,81 @@ EOF
    then
       rm -f /etc/yum.repos.d/TurboVNC.repo
    fi
-fi
+ fi
 
-dir=`dirname "$(readlink -f $0))"`
-if [ "$(rpm -q gftp | grep 'not installed' > /dev/null;echo $?)" == "0" ]
-then
+ dir=`dirname "$(readlink -f $0))"`
+ if [ "$(rpm -q gftp | grep 'not installed' > /dev/null;echo $?)" == "0" ]
+ then
    if [ -f $dir/linux/gftp-2.0.19-4.el6.rf.x86_64.rpm ]
    then
       yum -y install $dir/linux/gftp-2.0.19-4.el6.rf.x86_64.rpm &>> $logfile
    fi
-fi
-if [ "$(rpm -q numlockx | grep 'not installed' > /dev/null;echo $?)" == "0" ]
-then
+ fi
+ if [ "$(rpm -q numlockx | grep 'not installed' > /dev/null;echo $?)" == "0" ]
+ then
    if [ -f $dir/linux/numlockx-1.2-6.el7.nux.x86_64.rpm ]
    then
       yum -y install $dir/linux/numlockx-1.2-6.el7.nux.x86_64.rpm &>> $logfile
    fi
-fi
+ fi
 
-echo "CentOS / RedHat package installation complete"
-echo " "
+ echo "CentOS / RedHat package installation complete"
+ echo " "
+else
+ distrover=$(lsb_release -rs)
+ distmajor=${distrover:0:2}
+ if [ $distmajor -lt 14 ] ; then
+     echo "Only Ubuntu 14 or newer is supported"
+     echo " "
+     exit 1
+ fi
+ postfix=`date +"%F_%T"`
+ if [ -d /tmp/ovj_preinstall ]; then
+   logfile="/tmp/ovj_preinstall/pkgInstall.log_$postfix"
+ else
+   logfile="/tmp/pkgInstall.log_$postfix"
+ fi
+
+ echo "You can monitor the progress in a separate terminal window with the command"
+ echo "tail -f $logfile"
+ echo "Installing standard packages (1 of 2)"
+# The unattended-upgrade script often holds a yum lock.
+# This prevents this script from executing
+ npids=$(ps -ef  | grep unattended-upgrade | grep -v grep | awk '{ printf("%d ",$2) }')
+ for prog_pid in $npids
+ do
+   kill -s 2 $prog_pid
+ done
+ dpkg --add-architecture i386
+ apt-get -qq update
+# apt-get -qq -y dist-upgrade
+# Prevent packages from presenting an interactive popup
+ export DEBIAN_FRONTEND=noninteractive
+ apt-get install -y csh make expect bc git scons g++ gfortran \
+      openssh-server mutt sharutils sendmail-cf gnome-power-manager \
+      kdiff3 ghostscript imagemagick postgresql \
+      gedit dos2unix zip cups gnuplot gnome-terminal enscript &> $logfile
+ echo "Installing version specific packages (2 of 2)"
+ if [ $distmajor -gt 16 ] ; then
+   # these are needed to build
+    apt-get install -y gdm3 gnome-session openjdk-8-jre \
+      lib32stdc++-7-dev libc6-dev-i386 libglu1-mesa-dev libgsl-dev &>> $logfile
+ elif [ $distmajor -gt 14 ] ; then
+   # these are needed to build
+    apt-get install -y gdm3 gnome-session openjdk-8-jre \
+      lib32stdc++-5-dev libc6-dev-i386 libglu1-mesa-dev libgsl-dev &>> $logfile
+ else
+    # these are needed to build
+    apt-get install -y openjdk-6-jre lib32stdc++-4.8-dev \
+            libc6-dev-i386 libglu1-mesa-dev libgsl0-dev &>> $logfile
+ fi
+ # apt-get uninstalls these if an amd64 version is installed for something else >:(
+ # so install them last...
+ apt-get install -y libmotif-dev libx11-dev libxt-dev &>> $logfile
+ unset DEBIAN_FRONTEND
+ echo "dash dash/sh boolean false" | debconf-set-selections &>> $logfile
+ dpkg-reconfigure -u dash &>> $logfile
+ echo "Ubuntu package installation complete"
+ echo " "
+fi
 exit 0
