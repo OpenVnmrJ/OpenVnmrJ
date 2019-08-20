a7c5f7bad2dbd93414af8305b386495bff244988
Dan Iverson <daniverson2016@gmail.com>
2019-08-20 11:08:57 -0700

    dialog java file improved (#291)
    Added "password" input feature that masks input

--- src/dialog/src/dialog.java
+++ src/dialog/src/dialog.java
@@ -1,5 +1,9 @@
 
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JDialog;
 
 public class dialog {
     public static void main(String[] args) {
@@ -10,8 +14,33 @@ public class dialog {
            JOptionPane.showMessageDialog(null, args[0]);
         else
         {
-           String m = JOptionPane.showInputDialog(args[0]);
-           System.out.println(m);
+           if (args[1].startsWith("pass"))
+           {
+              JPanel panel = new JPanel();
+              JLabel label = new JLabel(args[0]);
+              JPasswordField pass = new JPasswordField(20);
+              panel.add(label);
+              panel.add(pass);
+              String[] options = new String[]{"OK", "Cancel"};
+              int option = JOptionPane.showOptionDialog(null, panel, "PassWord",
+                         JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
+                         null, options, options[0]);
+              if (option == 0)
+              {
+                 char[] password = pass.getPassword();
+                 System.out.println(new String(password));
+              }
+              else
+              {
+                 System.out.println("");
+              }
+           }
+           else
+           {
+               String m = JOptionPane.showInputDialog(args[0]);
+               System.out.println(m);
+           }
         }
     }
 }
+
