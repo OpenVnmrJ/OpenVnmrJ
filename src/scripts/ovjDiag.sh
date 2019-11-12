fba97954b1d6458f7e73d86cc96f41ca6ec94e7f
Dan Iverson <daniverson2016@gmail.com>
2019-11-12 10:15:28 -0800

    Changes message if OpenVnmrJ fails to start. (#304)
    Also provided ovjDiag to help diagnose the reason OpenVnmrJ
    failed to start.

---
+++ src/scripts/ovjDiag.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# Copyright (C) 2018  University of Oregon
+# 
+# You may distribute under the terms of either the GNU General Public
+# License or the Apache License, as specified in the LICENSE file.
+# 
+# For more information, see the LICENSE file.
+# 
+#
+# set -x
+
+ping -W 1 -c 1 $(hostname) > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo "Unable to ping $(hostname)"
+  echo "This hostname should be added to the /etc/hosts file"
+fi
+
+ldd /vnmr/bin/Vnmrbg | grep -i "not found" > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  echo "Some libraries appear to be missing."
+  echo "They appear as \"not found\" in the following output"
+  ldd /vnmr/bin/Vnmrbg
+  echo ""
+  echo "Use /vnmr/bin/installpkgs"
+  echo "to install the missing libraries"
+  echo ""
+fi
+
+echo ""
+echo "$0 complete"
