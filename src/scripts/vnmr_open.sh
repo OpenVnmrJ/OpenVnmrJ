c6b83344b3a7af9fecbf5f8a0416ce241c101b28
Dan Iverson <daniverson2016@gmail.com>
2020-01-13 13:39:03 -0800

    Updates from Krish (#350)
    Fixes to macros for pathnames longer than 256 characters.
    rotateannos did not honor changes to trace equal f1 or f2.
    vnmr_open complained of deprecated function.

--- src/scripts/vnmr_open.sh
+++ src/scripts/vnmr_open.sh
@@ -1,14 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 #
 # open a file based on type and default application for that type
-# e.g. open a pdf bring up Acroreader, a html file the browser
 #
 if [ -z "$vnmrsystem" ] ; then
    vnmrsystem="/vnmr"
 fi
-ostype=`uname -s`
+ostype=$(uname -s)
 path="$1"
-url=`perl $vnmrsystem/bin/addhttp.pl $1`
+url=$(perl $vnmrsystem/bin/addhttp.pl $1)
 if [ ! -z "$url" ] ; then
    path=$url
 fi
@@ -16,14 +15,16 @@ case "$ostype" in
    cygwin | Interix )
       opencmd="cmd /c start"
       if [ -z "$url" ]; then 
-         path=`unixpath2win "$1"`
+         path=$(unixpath2win "$1")
       fi
         ;;
    *inux*)   # Linux, linux, etc.
       # echo "linux"
-      # opencmd="gnome-open"  depricated
+      # opencmd="gnome-open"  deprecated
       # check for the replacement, if not present fail-back to gnome-open
-      if [ -x /usr/bin/xdg-open ]; then
+      if [ -x /usr/bin/gio ]; then
+         opencmd="gio open"
+      elif [ -x /usr/bin/xdg-open ]; then
          opencmd="xdg-open"
       else 
          opencmd="gnome-open"
@@ -35,7 +36,9 @@ case "$ostype" in
         ;;
       *)
        # echo "default"
-       if [ -x /usr/bin/xdg-open ]; then
+       if [ -x /usr/bin/gio ]; then
+         opencmd="gio open"
+       elif [ -x /usr/bin/xdg-open ]; then
          opencmd="xdg-open"
        else 
          opencmd="gnome-open"
