272c55a863a3b2f7ef4ed5ead7ec611f15a334c5
Dan Iverson <daniverson2016@gmail.com>
2021-12-17 14:53:44 -0800

    Converted vnmrauto from a csh to a bash script (#666)
    Removing .vnmrenv to fix the problem with NMRPipe and
    IST, issue #657, caused automation to fail since it relied
    on .vnmrenv. It now uses .vnmrenvsh

--- src/scripts/vnmrauto.sh
+++ src/scripts/vnmrauto.sh
@@ -1,5 +1,5 @@
-#! /bin/csh
+#!/bin/bash
 # Script used to run background automation. Called from Autoproc.
 
-source $HOME/.vnmrenv
+source $HOME/.vnmrenvsh
 Vnmrbg -mauto -hAutoproc -n1 -i42 -u$vnmruser autoqstart
