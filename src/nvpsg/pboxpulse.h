23b69091561c7e7787afa7546028466a28295135
Barry DeZonia <bdezonia@gmail.com>
2024-06-14 11:17:26 -0500

    Actually remove the buggy code

--- src/nvpsg/pboxpulse.h
+++ src/nvpsg/pboxpulse.h
@@ -810,9 +810,6 @@ void _pboxsim3pulse(PBOXPULSE shp1, PBOXPULSE shp2, PBOXPULSE shp3,
       psg_abort(1);
    }
 
-   // BDZ NOTE 6-13-24: there are six three channel cases below but only
-   // one of them does a delay(rof1). Is that a bug? See below.
-
    if ((chnl1 == 1) && (chnl2 == 2) && (chnl3 == 3)) {
       obsblank();
       decblank();
@@ -837,7 +834,13 @@ void _pboxsim3pulse(PBOXPULSE shp1, PBOXPULSE shp2, PBOXPULSE shp3,
          dec2unblank();
       delay(shp1.t1);
       delay(2.0e-6);
-      delay(rof1); // BDZ NOTE: 6-13-24: only this one case does this delay. Bug? 
+      
+      // BDZ 6-14-24: after discussion with Dan we removed this one line
+      //   as it appears to be a bug. Of the six subcases in this routine
+      //   only this subcase had this line.
+      //
+      //delay(rof1);
+       
       if (NUMch > 2) {
          sim3shaped_pulse(shp1.pattern,shp2.pattern,shp3.pattern,shp1.pw,
                           shp2.pw,shp3.pw,phase1,phase2,phase3,0.0,0.0);
