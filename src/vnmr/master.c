27894f5f15f2b93a50ab02cb77b866761407dd62
yaleks <yaleks@gmail.com>
2026-07-29 08:15:12 -0400

    vnmr/master.c - revert

--- src/vnmr/master.c
+++ src/vnmr/master.c
@@ -44,16 +44,15 @@ int     debug = 0;
 #endif  DEBUG
 
 /*  Definition of the acquisition message entry uses trickery in
-    that the actual entry may be smaller than 1024+4 bytes.  The
+    that the actual entry may be smaller than 256+4 bytes.  The
     actual size is determined by the length of the message received
     from Acqproc.  The structure definition is only referenced by
-    pointers and never explicitly allocated. NOTE: must stay in sync
-    with smagic.c's struct acqmsgentry (curentry[ACQMBUFSIZE]==1024).			*/
+    pointers and never explicitly allocated.			*/
 
 struct acqmsgentry
 {
    char           *nextentry;
-   char            curentry[1024];
+   char            curentry[256];
 };
 
 
@@ -321,7 +320,7 @@ char           *msg;
 showDispBg(msg)
 char           *msg;
 {
-   fprintf(stderr, "master: reference made to non-existant background field!\n");
+   fprintf(stderr, "master: reference made to non-existant background field!\n");
 }
 
 showDispSeq(msg)
